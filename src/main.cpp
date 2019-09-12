b6a9e735b227f15796b0372d60ecef7035e1df2d
William Longpre <williamlongpre@hotmail.com>
2019-09-12 10:18:40 -0400

    Hello world
    ceci est un test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("yes");
+  delay(1000);
 }
